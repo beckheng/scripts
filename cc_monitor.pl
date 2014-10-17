0de954a4c6aadf48e46466caea70d3ec9fb5517d
Beckheng Lam <beckheng@gmail.com>
2014-10-17 10:18:38 +0800

    drush cc命令可以带参数指定清除类型

--- cc_monitor.pl
+++ cc_monitor.pl
@@ -1,7 +1,7 @@
 #!perl
 
 # 监控drupal sites下的module文件，有改变则用drush cc清除cache
-# 此脚本需要在drupal_site_path对应的目录下执行，不然IPC通信结果不正确
+# 此脚本需要在drupal_site_path对应的目录下执行
 # usage: 
 # 1. cd the_drupal_site_path
 # 2. perl $0 path1 path2 path..n
@@ -9,7 +9,6 @@
 use strict;
 
 use File::ChangeNotify;
-use IPC::Open3;
 
 sub usage
 {
@@ -22,8 +21,6 @@ if (!@site_path)
 	&usage();
 }
 
-local(*CIN, *COUT, *CERR);
-
 my $watcher = File::ChangeNotify->instantiate_watcher(
 	directories => [@site_path],
 	filter => qr/\.(?:module|inc|info)$/,
@@ -35,18 +32,6 @@ print "monitor @site_path, wait for events\n\n";
 while (my @events = $watcher->wait_for_events())
 {
 	print "\nclear cache at: " . scalar(localtime()) . "\n";
-	my $pid = open3(*CIN, *COUT, *CERR, 'drush cc');
-	print CIN "1\n"; # choose clear all cache
-	close(CIN);
-	my @outlines = <COUT>;
-	my @errlines = <CERR>;
-	
-	print "STDOUT:\n", @outlines, "\n";
-	print "STDERR:\n", @errlines, "\n\n";
-	
-	close(COUT);
-	close(CERR);
-
-	waitpid($pid, 0);
+	system('drush cc all');	
+	print "\n";
 }
-
