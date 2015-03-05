d270b9d731700d2339d310c5f63ac822f28cd082
Beckheng Lam <beckheng@gmail.com>
2015-03-05 10:42:41 +0800

    clear cache

--- symfony-build-bootstrap-php.sh
+++ symfony-build-bootstrap-php.sh
@@ -1,3 +1,6 @@
-#!bash
+#!/bin/bash
 
 vendor/sensio/distribution-bundle/Sensio/Bundle/DistributionBundle/Resources/bin/build_bootstrap.php
+
+php app/console cache:clear --no-warmup --no-optional-warmers --env=dev
+php app/console cache:clear --no-warmup --no-optional-warmers --env=prod
